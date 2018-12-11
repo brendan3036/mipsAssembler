4447e74b7c530fa49827dd97530c9e0502afb4ac
Brendan Karper <bk3036@cs.ship.edu>
2018-12-11 17:44:39 -0500

    no commas anymore

--- assembler.asm
+++ assembler.asm
@@ -1,10 +1,10 @@
-add $t0, $t1, $t2
-sw $t0, 0($s0)
-lw $t3, 0($s0)
-addi $t0, $s1, 5
+add $t0 $t1 $t2
+sw $t0 0($s0)
+lw $t3 0($s0)
+addi $t0 $s1 5
 j L1
-sub $t0, $t1, $t3
-sll $s3, $t0, 2
-slt $s0, $t0, $t1
-and $s2, $t0, $t1
-or $s1, $t0, $t1
+sub $t0 $t1 $t3
+sll $s3 $t0 2
+slt $s0 $t0 $t1
+and $s2 $t0 $t1
+or $s1 $t0 $t1
